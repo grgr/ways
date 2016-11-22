5f4dbd0830fa14fda01006a1ef084031d71c4f4a
cgregor <chrgregor@gmail.com>
2016-11-22 14:36:07 +0100

    preparing params

--- lib/ways.rb
+++ lib/ways.rb
@@ -1,13 +1,57 @@
 module Ways
-  def self.hello
-    "Hello"
-  end
-
-  def self.setup
-    yield self
-  end 
 
   mattr_accessor :api
+  mattr_accessor :api_url
   mattr_accessor :api_access_id
+  mattr_accessor :api_access_id_key 
+  mattr_accessor :api_lang_key 
+  mattr_accessor :api_origin_lat_key
+  mattr_accessor :api_origin_long_key
+  mattr_accessor :api_dest_lat_key
+  mattr_accessor :api_dest_long_key
+  mattr_accessor :api_time_key
+  mattr_accessor :api_date_key
+  mattr_accessor :api_arrival_bool_key
+
+  class << self
+    def hello
+      "Hello"
+    end
+
+    def setup
+      yield self
+    end 
+
+    def from_to(from, to, time, date, lang='de', opts={})
+    end
+
+    def prepare_results(from, to, time, date, lang, opts)
+    end
+
+    def get_results(from, to, time, date, lang, opts)
+      url = [api_url, parametrize(from, to, time, date, lang, opts)].join('?')
+    end
+
+    def parametrize(from, to, time, date, lang, opts)
+      time ||= Time.now
+      date ||= Date.today
+
+      params = {
+        "#{api_access_id_key}" =>    api_access_id,
+        "#{api_lang_key}" =>         lang,
+        "#{api_origin_lat_key}" =>   from[app_lat_key],
+        "#{api_origin_long_key}" =>  from[app_long_key],
+        "#{api_dest_lat_key}" =>     to[app_lat_key],
+        "#{api_dest_long_key}" =>    to[app_long_key],
+        "#{api_time_key}" =>         time,
+        "#{api_date_key}" =>         date,
+        "#{api_arrival_bool_key}" => arrival
+      }
+      
+      params.update( "#{api_origin_walk_key}" => opts[:origin_walk] ) if opts[:origin_walk]
+
+      params.to_query
+    end
+  end
 
 end
--- spec/test_app/config/initializers/ways.rb
+++ spec/test_app/config/initializers/ways.rb
@@ -1,6 +1,17 @@
 Ways.setup do |config|
 
   config.api = :vbb
+  config.api_url = "http://demo.hafas.de/openapi/vbb-proxy"
   config.api_access_id = ENV['VBB_ACCESS_ID']
+  config.api_access_id_key = 'accessID'
+  config.api_lang_key = 'lang'
+  config.api_origin_lat_key = 'originCoordLat'
+  config.api_origin_long_key = 'originCoordLong'
+  config.api_dest_lat_key = 'destCoordLat'
+  config.api_dest_long_key = 'destCoordLong'
+  config.api_time_key = 'time'
+  config.api_date_key = 'date'
+  config.api_arrival_bool_key = 'searchForArrival'
+  config.api_origin_walk_key = 'originWalk'
 
 end
