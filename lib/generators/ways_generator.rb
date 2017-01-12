4856424ea5b14596d18314be5f5eb1ddc3de14e8
cgregor <chrgregor@gmail.com>
2017-01-12 11:46:18 +0100

    changing generator to use which-api param

--- lib/generators/ways_generator.rb
+++ lib/generators/ways_generator.rb
@@ -1,12 +1,12 @@
 require 'rails/generators/base'
 
-class WaysGenerator < Rails::Generators::Base
-  desc "This generator creates an initializer file at config/initializers"
+class WaysGenerator < Rails::Generators::NamedBase
+  desc "This generator creates an initializer either for vbb or hvv at config/initializers"
 
   source_root File.expand_path("../templates", __FILE__)
 
   def copy_initializer_file
-    copy_file "initializer.rb", "config/initializers/ways.rb"
+    copy_file "#{name}.rb", "config/initializers/ways.rb"
   end
 
 end
